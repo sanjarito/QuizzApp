fc5a0c1eb12b21aab4a0b5fc7ea70f26631e272b
Santiago Casar <santiago@nowhereprod.com>
2019-02-19 10:37:23 -0500

    finished first version of climbing quiz

---
+++ data.js
@@ -0,0 +1,124 @@
+const QUESTIONS = [
+  {
+    "status":"hidden",
+    "number":1,
+    "question":"When you climb outside playing loud music is:",
+    "answers":[{
+      "A":"Not Cool",
+      "B":"Distracting",
+      "C":"An element of Risk",
+      "D":"All of the above"
+    }],
+    "correct":"All of the above"
+  },
+  {
+    "status":"hidden",
+    "number":2,
+    "question":"First step to do before climbing outside:",
+    "answers":[{
+      "A":"Get aquainted to the area",
+      "B":"Buy a guidebook",
+      "C":"Post on facebook about it",
+      "D":"Nothing"
+    }],
+    "correct":"Get aquainted to the area"
+  },
+  {
+    "status":"hidden",
+    "number":3,
+    "question":"When you climb outside you should always:",
+    "answers":[{
+      "A":"Go with someone",
+      "B":"Pack sandwiches",
+      "C":"Post about it on instagram",
+      "D":"None of the above"
+    }],
+    "correct":"Go with someone"
+  },
+  {
+    "status":"hidden",
+    "number":4,
+    "question":"The main difference between climbing inside vs outside is:",
+    "answers":[{
+      "A":"Outside is harder",
+      "B":"Outside routes are not marked",
+      "C":"Outside is more dangerous",
+      "D":"More people outside"
+    }],
+    "correct":"Outside routes are not marked"
+  },
+  {
+    "status":"hidden",
+    "number":5,
+    "question":"What is not allowed outside?",
+    "answers":[{
+      "A":"Chalk",
+      "B":"Brushing",
+      "C":"Taping Holds as markers",
+      "D":"Stick cliping"
+    }],
+    "correct":"Taping Holds as markers"
+  },
+  {
+    "status":"hidden",
+    "number":6,
+    "question":"Most common type of hold outside",
+    "answers":[{
+      "A":"Crimps",
+      "B":"Slopers",
+      "C":"Pinches",
+      "D":"none of the above"
+    }],
+    "correct":"none of the above"
+  },
+  {
+    "status":"hidden",
+    "number":7,
+    "question":"What are three items required to go sport climbing outside",
+    "answers":[{
+      "A":"shoes, helmet, quickdraws",
+      "B":"belay-device, rope, quickdraws",
+      "C":"cd-player, peanuts, water",
+      "D":"rope, cheetos, water"
+    }],
+    "correct":"belay-device,rope,quickdraws"
+  },
+  {
+    "status":"hidden",
+    "number":8,
+    "question":"Which best describes climbing outside",
+    "answers":[{
+      "A":"Easy",
+      "B":"Hard",
+      "C":"Dangerous",
+      "D":"Skin Shredder"
+    }],
+    "correct":"Skin Shredder"
+  },
+  {
+    "status":"hidden",
+    "number":9,
+    "question":"Which is not a climbing term",
+    "answers":[{
+      "A":"slimper",
+      "B":"Gri-gri",
+      "C":"Venga",
+      "D":"cheetos"
+    }],
+    "correct":"cheetos"
+  },
+  {
+    "status":"hidden",
+    "number":10,
+    "question":"When leaving a crag you must:",
+    "answers":[{
+      "A":"Post on facebook",
+      "B":"Post on instagram",
+      "C":"Dance",
+      "D":"Leave no trace behind"
+    }],
+    "correct":"Leave no trace behind"
+  },
+
+
+]
--- main.js
+++ main.js
@@ -1,1 +1,88 @@
-console.log("Asdasd")
+'use strict';
+
+let questionNo = 0;
+let score=0;
+const totalQuestions = 10;
+
+
+  function startQuiz(){
+
+    $('button').on('click', '.go-button', function (event) {
+      event.preventDefault()
+      $('.start').fadeOut()
+      // $('.container').append('<h2 class="score">Score : '+score)
+      // $('.container').append('<h2 class="questionNo">'+questionNo + " out of : "+totalQuestions+'</h2>')
+      createQuestions(questionNo)
+
+
+    });
+  }
+
+
+    function createQuestions(questionNo){
+        $('.container').find('.score').html(`<h2 class="green">score : ${score}</h2>`)
+        $('.container').find('.question-No').html(`<h2 class="green">Question ${questionNo} out of 10</h2>`)
+
+        $('.quiz-questions').append('<li class="question">' + QUESTIONS[questionNo]["question"] + '</li>')
+        $('#js-climbing-quiz').append(`
+        <button class="choice"><p>A</p></button>
+        <button class="choice"><p>B</p></button>
+        <button class="choice"><p>C</p></button>
+        <button class="choice"><p>D</p></button>
+        `)
+
+        $('button').hover(function(e){
+          e.stopPropagation()
+          let letterVariable = $(this).children().text()
+          $(this).addClass('button_hover')
+          $(this).children().text(QUESTIONS[questionNo]["answers"][0][letterVariable])
+        })
+
+
+        $('#js-climbing-quiz').one('click','.choice', function(e){
+          e.preventDefault();
+          let user_choice = $(this).children().text()
+          let correct_answer = QUESTIONS[questionNo]["correct"]
+          if (user_choice == correct_answer && questionNo != 9) {
+            questionNo += 1
+            $( "" ).replaceAll( ".question" );
+            $( "" ).replaceAll( ".choice" );
+            console.log("right answer");
+            score +=1
+            createQuestions(questionNo)
+          } else if (user_choice != correct_answer && questionNo != 9) {
+            console.log("wrong answer")
+            questionNo += 1
+            $( "" ).replaceAll( ".question" );
+            $( "" ).replaceAll( ".choice" );
+            createQuestions(questionNo)
+          } else {
+            if (user_choice == correct_answer){
+              score +=1
+            }
+            endScreen(score)
+          }
+        })
+    };
+
+  function endScreen(score){
+    console.log(score)
+  $('.container').hide()
+    if(score >= 8){
+      $('body').append(
+        `<h1> You did great, end screen<h1>`
+      )
+    } else {
+      $('body').append(
+        `<h1> You did aweful, end screen<h1>`
+      )
+    }
+  }
+
+
+  function startgame(){
+    startQuiz()
+
+  }
+
+startgame();
